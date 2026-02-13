5acc5f5cdc17c401fff7a720dde97546ce6d60f3
Giacomo C. <giacomo.corso@gmail.com>
2026-02-13 16:29:45 +0100

    feat: updated Memory controller

--- src/main/java/dev/giaco/springai/memory/MemoryController.java
+++ src/main/java/dev/giaco/springai/memory/MemoryController.java
@@ -1,6 +1,8 @@
 package dev.giaco.springai.memory;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +13,10 @@ public class MemoryController {
 
     private final ChatClient chatClient;
 
-    public MemoryController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public MemoryController(ChatClient.Builder builder, ChatMemory chatMemory) {
+        this.chatClient = builder
+        .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+        .build();
     }
 
     @GetMapping("/memory")
